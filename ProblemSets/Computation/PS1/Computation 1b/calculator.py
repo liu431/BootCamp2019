7ddbde59292b13ded5e28249409dfd08181869b9
liu431 <lliu95877@gmail.com>
2019-07-08 22:54:44 -0500

    'add'

---
+++ ProblemSets/Computation/PS1/Computation 1b/calculator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 11:51:00 2019
+
+@author: LI LIU
+"""
+
+def sum(a,b):
+    return a+b
+
+def pdt(a,b):
+    return a*b
+
+if __name__=='__main__':
+    sum(a,b)
+    
